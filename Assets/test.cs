3f39a30e5681030a7d1de5b983b29ea3bd07dac6
kou.fujita <pukurinawaikeda@icloud.com>
2020-02-24 18:31:53 +0900

    Script testの課題
    Lesson4の課題です

---
+++ Assets/test.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class test : MonoBehaviour {
+
+  
+    
+// Use this for initialization
+void Start(){
+        
+
+        int[] points = { 5, 3, 7, 2, 9 };
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(points[i]);
+        }
+
+
+        for (int i=4; i>-1; i--)
+        {
+            Debug.Log(points[i]);
+        
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
